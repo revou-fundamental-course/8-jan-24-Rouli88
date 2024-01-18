256ec8613f9be6bff25c0ba9ba0e04d54cf8958c
Rouli Sinaga <roulisinaga@Roulis-MacBook-Pro.local>
2024-01-18 17:56:37 +0700

    first push

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function luas() {
+var s = document.getElementById ('squareside').value;
+var luas = s*s;
+document.getElementById('luas').innerHTML
+}
+
+function keliling() {
+    var s = document.getElementById ('squareside').value;
+    var keliling = 4*s;
+    document.getElementById('Keliling').inserthtml = keliling
+}
